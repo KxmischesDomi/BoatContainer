ea3ff6a5c4a48f2f6984d820f9f3cf97feedacb7
KxmischesDomi <kxmischesdomi@gmail.com>
2021-09-09 19:35:26 +0200

    Add assets

--- src/main/java/de/kxmischesdomi/boatcontainer/BoatContainer.java
+++ src/main/java/de/kxmischesdomi/boatcontainer/BoatContainer.java
@@ -6,7 +6,7 @@ import net.fabricmc.api.ModInitializer;
 
 public class BoatContainer implements ModInitializer {
 
-	public static final String MOD_ID = "customboats";
+	public static final String MOD_ID = "boatcontainer";
 
 	@Override
 	public void onInitialize() {
