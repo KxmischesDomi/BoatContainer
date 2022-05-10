dc852f2865f1bcf8276478ebd37afb338a403b22
KxmischesDomi <kxmischesdomi@gmail.com>
2022-05-10 22:34:20 +0200

    Implement dispenser behaviour

--- src/main/java/de/kxmischesdomi/boatcontainer/common/registry/ModItems.java
+++ src/main/java/de/kxmischesdomi/boatcontainer/common/registry/ModItems.java
@@ -4,16 +4,26 @@ import com.mojang.datafixers.util.Function5;
 import de.kxmischesdomi.boatcontainer.BoatContainer;
 import de.kxmischesdomi.boatcontainer.common.entity.ChestBoatEntity;
 import de.kxmischesdomi.boatcontainer.common.entity.EnderChestBoatEntity;
+import de.kxmischesdomi.boatcontainer.common.entity.FurnaceBoatEntity;
 import de.kxmischesdomi.boatcontainer.common.entity.OverriddenBoatEntity;
 import de.kxmischesdomi.boatcontainer.common.item.CustomBoatItem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.BlockSource;
+import net.minecraft.core.Direction;
 import net.minecraft.core.Registry;
+import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.core.dispenser.DispenseItemBehavior;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.Boat.Type;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.DispenserBlock;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +36,7 @@ public class ModItems {
 
 	public static CustomBoatItem[] CHEST_BOAT = registerBoat("chest_boat", ModEntities.CHEST_BOAT, ChestBoatEntity::new);
 	public static CustomBoatItem[] ENDER_CHEST_BOAT = registerBoat("ender_chest_boat", ModEntities.ENDER_CHEST_BOAT, EnderChestBoatEntity::new);
+	public static CustomBoatItem[] FURNACE_BOAT = registerBoat("furnace_boat", ModEntities.FURNACE_BOAT, FurnaceBoatEntity::new);
 
 	public static void init() {}
 
@@ -36,6 +47,7 @@ public class ModItems {
 		for (Type value : Type.values()) {
 			CustomBoatItem item = register(value.getName() + "_" + name, new CustomBoatItem(type, instanceCreator, value, new Properties().stacksTo(1).tab(CreativeModeTab.TAB_TRANSPORTATION)));
 			list.add(item);
+			registerBoatDispenserBehavior(item, type, instanceCreator);
 		}
 
 		return list.toArray(new CustomBoatItem[0]);
@@ -44,6 +56,50 @@ public class ModItems {
 	public static <T extends Item> T register(String name, T item) {
 		Registry.register(Registry.ITEM, new ResourceLocation(BoatContainer.MOD_ID, name), item);
 		return item;
+	}
+
+	public static void registerBoatDispenserBehavior(ItemLike item, EntityType<? extends OverriddenBoatEntity> entityType, Function5<EntityType<? extends OverriddenBoatEntity>, Level, Double, Double, Double, ? extends OverriddenBoatEntity> instanceCreator) {
+		DispenserBlock.registerBehavior(item, new CustomBoatDispenserBehavior(entityType, instanceCreator));
+	}
+
+	public static class CustomBoatDispenserBehavior extends DefaultDispenseItemBehavior {
+
+		private static final DispenseItemBehavior FALLBACK_BEHAVIOR = new DefaultDispenseItemBehavior();
+		private static final float OFFSET_MULTIPLIER = 1.125F;
+
+		private final EntityType<? extends OverriddenBoatEntity> entityType;
+		private final Function5<EntityType<? extends OverriddenBoatEntity>, Level, Double, Double, Double, ? extends OverriddenBoatEntity> instanceCreator;
+
+		public CustomBoatDispenserBehavior(EntityType<? extends OverriddenBoatEntity> entityType, Function5<EntityType<? extends OverriddenBoatEntity>, Level, Double, Double, Double, ? extends OverriddenBoatEntity> instanceCreator) {
+			this.entityType = entityType;
+			this.instanceCreator = instanceCreator;
+		}
+
+		@Override
+		public ItemStack execute(BlockSource pointer, ItemStack stack) {
+			Direction facing = pointer.getBlockState().getValue(DispenserBlock.FACING);
+
+			double x = pointer.x() + facing.getStepX() * OFFSET_MULTIPLIER;
+			double y = pointer.y() + facing.getStepY() * OFFSET_MULTIPLIER;
+			double z = pointer.z() + facing.getStepZ() * OFFSET_MULTIPLIER;
+
+			Level world = pointer.getLevel();
+			BlockPos pos = pointer.getPos().relative(facing);
+
+			if (world.getFluidState(pos).is(FluidTags.WATER)) {
+				y += 1;
+			} else if (!world.getBlockState(pos).isAir() || !world.getFluidState(pos.below()).is(FluidTags.WATER)) {
+				return FALLBACK_BEHAVIOR.dispense(pointer, stack);
+			}
+
+			OverriddenBoatEntity boatEntity = instanceCreator.apply(entityType, world, x, y, z);
+
+			boatEntity.setYRot(facing.toYRot());
+			world.addFreshEntity(boatEntity);
+
+			stack.shrink(1);
+			return stack;
+		}
 	}
 
 }
